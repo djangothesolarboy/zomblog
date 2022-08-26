5fb04a796d581c003f9e69a9f4db1b44d42f66d0
django <damienjdarko@gmail.com>
2022-08-26 15:21:25 -0400

    :zap:

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const Post = require('./../models/post');
 const router = express.Router();
 
 router.get('/new', (req, res) => {
     res.render('posts/new')
 });
 
-router.post('/', (req, res) => {
+router.get('/:id', (req, res) => {
 
+})
+
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown,
+    });
+
+    try {
+        post = await post.save();
+        res.redirect(`/posts/${post.id}`);
+    } catch (e) {
+        res.render('posts/new', { post: post });
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 mongoose.connect('mongodb://localhost/blog');
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 app.use('/posts', postRouter);
+
 app.get('/', (req, res) => {
     const posts = [{
         title: 'test',
